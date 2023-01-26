8e630313c3dde769a13c077a375bd023831a628e
Gamja3 <44pico@naver.com>
2023-01-26 19:09:09 +0900

    데이터베이스 추가

--- src/fbBase.js
+++ src/fbBase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -15,3 +16,4 @@ const app = initializeApp(firebaseConfig);
 export const authService = getAuth();
 
 export const firebaseInstance = initializeApp;
+export const dbService = getFirestore();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,15 +1,37 @@
-import React from "react";
+import { dbService } from "fbBase";
+import React, { useState } from "react";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 
 function Home(props) {
+    const [nweet, setNweet] = useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+        await addDoc(collection(dbService, "nweets"), {
+            nweet,
+            createdAt: serverTimestamp(),
+        });
+
+        setNweet("");
+    };
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setNweet(value);
+    };
     return (
         <div>
-            <form>
+            <form onSubmit={onSubmit}>
                 <input
+                    value={nweet}
+                    onChange={onChange}
                     type="text"
                     placeholder="What's on your mind?"
                     maxLength={120}
                 />
-                <input type="submit" value="Nweet" />
+                <input type="submit" value="nweet" />
             </form>
         </div>
     );
